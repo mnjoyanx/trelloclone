77a874a7ac3f2a13d7bed95ff31b8f276bf8c63b
mnjoyanx <tigranmnjoyan@gmail.com>
2021-03-09 22:52:26 +0400

    tailwindcss and vue are connected

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
+ const tailwindcss = require('tailwindcss');
+
+
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss('./tailwind.config.js')]
+     });
+
+     mix.version()
